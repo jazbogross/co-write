8b0d37a69f6cfb864b6859829bf1771aa3f89122
jazbogross <57679104+jazbogross@users.noreply.github.com>
2025-02-23 17:47:36 +0000

    octokit version

--- supabase/functions/handle-suggestion-status/index.ts
+++ supabase/functions/handle-suggestion-status/index.ts
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.4'
-import { Octokit } from 'https://esm.sh/octokit@19.0.4'
+import { Octokit } from 'https://esm.sh/octokit@21.1.1'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
