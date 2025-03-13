436c48b6beff79c62d3ea45b7c408e699a382b02
jazbogross <57679104+jazbogross@users.noreply.github.com>
2025-03-13 14:18:23 +0000

    fix authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import Auth from "@/pages/Auth";
@@ -7,19 +6,22 @@ import ScriptEdit from "@/pages/ScriptEdit";
 import NotFound from "@/pages/NotFound";
 import GitHubCallback from "@/pages/GitHubCallback";
 import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "./hooks/useAuth";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/scripts/:id" element={<ScriptEdit />} />
-        <Route path="/github/callback" element={<GitHubCallback />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/scripts/:id" element={<ScriptEdit />} />
+          <Route path="/github/callback" element={<GitHubCallback />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+      </AuthProvider>
     </Router>
   );
 }
